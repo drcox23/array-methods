8e16166fea4b19fb26c774dc6cd7171c9c6ffb0d
doug <drcox23@gmail.com>
2018-06-12 19:49:01 -1000

    initial commit

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,15 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+var nameArray = [];
+var newNamelength = nameArray.push("Doug", "Cox");
+console.log(nameArray);
+
+var newGenericNumberArrayLength = genericNumberArray.push(6, 7, 8, 9, 10);
+console.log(genericNumberArray);
+
+
+
+
+
